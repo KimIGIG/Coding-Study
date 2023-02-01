d87d30f9af7319a8aac8f3af9202a1860df8274f
KimIGIG <zzang2033@gmail.com>
2023-02-01 23:35:32 +0900

    Create bj14888.py

---
+++ Baekjoon/bj14888.py
@@ -0,0 +1,71 @@
+import sys
+from itertools import permutations
+
+input = sys.stdin.readline
+
+N = int(input())
+
+num = list(map(int, input().split()))
+
+# +, -, *, /
+op = list(map(int, input().split()))
+op_list = ['+', '-', '*', '/']
+space = []
+
+# 있는 연산자들만 모으기
+for k in range(len(op)):
+    for i in range(op[k]):
+        space.append(op_list[k])
+
+#최대, 최소 기준
+maximum = -1e9
+minimum = 1e9
+
+def dfs(depth , total, plus, minus, multiply, divide):
+    global maximum
+    global minimum
+
+    if depth == N:
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
+        return
+    
+    if plus:
+        dfs(depth + 1, total + num[depth], plus - 1, minus, multiply, divide)
+    if minus:
+        dfs(depth + 1, total - num[depth], plus, minus - 1, multiply, divide)
+    if multiply:
+        dfs(depth + 1, total * num[depth], plus, minus, multiply - 1, divide)
+    if divide:
+        dfs(depth + 1, int(total / num[depth]), plus, minus, multiply, divide - 1)
+
+#dfs(1, num[0], op[0], op[1], op[2], op[3])
+#print(maximum)
+#print(minimum)
+
+def solve():
+    global maximum, minimum
+
+    for case in permutations(space, N-1):
+        total = num[0]
+        print(case)
+        for r in range(1, N):
+            if case[r-1] == '+':
+                total += num[r]
+            elif case[r-1] == '-':
+                total -= num[r]
+            elif case[r-1] == '*':
+                total *= num[r]
+            elif case[r-1] == '/':
+                total = int(total / num[r])
+
+        if total > maximum:
+            maximum = total
+        if total < minimum:
+            minimum = total
+
+solve()
+print(maximum)
+print(minimum)
+# 출력 1 : 최댓값
+# 출력 2 : 최솟값
