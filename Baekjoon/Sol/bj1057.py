b94b46c06898d0f497c2d18a5f4a16acf8544b5f
KimIGIG <zzang2033@gmail.com>
2023-02-09 03:55:40 +0900

    bj1057

---
+++ Baekjoon/Sol/bj1057.py
@@ -0,0 +1,15 @@
+import sys
+from math import log2
+
+n, a, b = map(int, input().split())
+
+ans = -1
+for round in range(1,int(log2(n)) + 2):
+
+    if (abs(a-b) == 1) and min(a,b) % 2:
+        ans = round
+        break
+    a = (a+1) // 2 
+    b = (b+1) // 2 
+
+print(ans)
--- Baekjoon/Sol/bj2559.py
+++ Baekjoon/Sol/bj2559.py
@@ -1,16 +1,35 @@
-# 투 투포인터 알고리즘 문제
-# 백준 2559 문제
-# 연속된 며칠 동안의 온도 합 최대값 찾기
+'''
+구간합 구하기
+슬라이싱 [i:i+k] 와 같이 구하면 슬라이싱에 해당하는 원소들을 복사하여 새로운 객체를 생성하기에 원소수에 비례하는 시간이 걸린다.
 
+tip.
+1. 가장 처음의 구간합을 구한다.
+2. 첫번째 원소는 빼고 다음 원소는 더하는 식으로 구간합을 이어가면서 최대값 찾는다.
+'''
 import sys
+
 input = sys.stdin.readline
 
-result =[]
+# n: 온도 측정 전체 날짜 (2이상) N개의 온도라고 이해
+# k: 연속적인 날짜의 수 (1과 N)
+n, k = map(int, input().split())
 
-n, k = map(int,input().split())
-a = list(map(int, input().split()))
+day = list(map(int, input().split()))
 
-for i in range(n - k + 1):
-    result.append(sum(a[i:i+k]))
-        
-print(max(result))
+
+cur = sum(day[:k])
+ans = cur
+
+# k부터 n까지 (2부터 10까지)
+for i in range(k, n):
+    #void.append(sum(day[i:i+k])) #  슬라이싱은 복사때문에 원소가 많을수록 오래걸린다.
+    #     #max_temp = max(max_temp, sum(day[i:i+k]))
+    
+    # 첫원소 빼고 마지막원소 더하기
+    #cur = cur + 
+    #print(i, i-k)
+    cur = cur - day[i-k] + day[i]
+    ans = max(cur, ans)
+
+print(ans)
+#print(max(void))
